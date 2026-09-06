e3118bb24d265901d2f36020aad2b0eaf4e20d40
Anisul Haque <anisulhaque773@gmail.com>
2026-09-06 22:32:52 +0200

    a little change in lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "../../generated/prisma/client";
 
 const connectionString = config.databaseUrl
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export {prisma};
